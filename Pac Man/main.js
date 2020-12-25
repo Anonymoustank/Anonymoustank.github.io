b90d56c357d9825a211ada8d8370373ee264195d
Anonymoustank <pkadekodi@gmail.com>
2020-12-24 21:54:01 -0700

    Added primary stages of pathfinding

--- Pac Man/main.js
+++ Pac Man/main.js
@@ -6,25 +6,68 @@ var ctx = canvas.getContext("2d");
 var dead = false;
 var keyBeingPressed = false
 var hasStarted = false
+var previous_key = false
 document.addEventListener("keydown", keyDownHandler, false);
 var speed = 5;
 var potential_move = []
-var nodes = []
+var nodes = new Set()
 var starting_position = 300
-for (let i = starting_position; i <= starting_position + 900; i = i + 5){
-    if (i === 100){
-        for (let j = 100; j <= 250; j = j + 5){
-            nodes.push([i, j])
-        }
+var starting_y_position = 100
+
+class node {
+    constructor(x, y, connecting_nodes = []){
+        this.x = x
+        this.y = y
+        this.connecting_nodes = connecting_nodes
     }
-    else if (i === starting_position + 200){
-        for (let j = 100; j <= 500; j = j + 5){
-            nodes.push([i, j])
+}
+for (let i = starting_position; i <= starting_position + 900; i = i + 5){
+    if (i === starting_position + 200){
+        for (let j = starting_y_position; j <= starting_y_position + 400; j = j + 5){
+            eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+            eval('nodes.add(' + "node" + i + j + ')')
+            // nodes.push(new node(i, j))
         }
     }
     else {
-        nodes.push([i, 100])
+        // nodes.push(new node(i, starting_y_position))
+        eval('var node' + i + starting_y_position + "= new node(" + i + "," + starting_y_position + ")")
+        eval('nodes.add(' + "node" + i + starting_y_position + ')')
     }
+    // nodes.push(new node(i, starting_y_position + 550))
+    eval('var node' + i + (starting_y_position + 550) + "= new node(" + i + "," + (starting_y_position + 550) + ")")
+    eval('nodes.add(' + "node" + i + (starting_y_position + 550) + ')')
+}
+
+for (let node of nodes){
+    try {
+        eval('var test_node1 = node' + (node.x + 5) + node.y)
+        if (nodes.has(test_node1)){
+            node.connecting_nodes.push(test_node1)
+        }
+    }
+    catch (e){}
+    try{
+        eval('var test_node2 = node' + (node.x - 5) + node.y)
+        if (nodes.has(test_node2)){
+            node.connecting_nodes.push(test_node2)
+        }
+    }
+    catch (e){}
+    try {
+        eval('test_node3 = node' + node.x + (node.y + 5))
+        if (nodes.has(test_node3)){
+            node.connecting_nodes.push(test_node3)
+        }
+    }
+    catch (e){}
+    try{
+        eval('test_node4 = node' + node.x + (node.y - 5))
+        if (nodes.has(test_node4)){
+            node.connecting_nodes.push(test_node4)
+        }
+    }
+    catch (e){}
 }
 
 class GameObject {
@@ -41,16 +84,20 @@ class GameObject {
 
 function keyDownHandler(e) {
     if (running == false){
-        if(e.key == "Right" || e.key == "ArrowRight" || e.key == "d") {
+        if(e.key == "Right" || e.key == "ArrowRight" || e.key == "d"){
+            previous_key = keyBeingPressed
             keyBeingPressed = "rightPressed" 
         }
-        else if(e.key == "Left" || e.key == "ArrowLeft" || e.key == "a") {
+        else if(e.key == "Left" || e.key == "ArrowLeft" || e.key == "a"){
+            previous_key = keyBeingPressed
             keyBeingPressed = "leftPressed"
         }
         else if (e.key == "Down" || e.key == "ArrowDown" || e.key == "s"){
+            previous_key = keyBeingPressed
             keyBeingPressed = "downPressed"
         }
         else if (e.key == "Up" || e.key == "ArrowUp" || e.key == "w"){
+            previous_key = keyBeingPressed
             keyBeingPressed = "upPressed"
         }
     }
@@ -130,7 +177,7 @@ class GIF extends GameObject{
     
 }
 
-var player = new GIF(400, 400, [["Images/PlayerUP - 1.png", "Images/PlayerUP - 2.png"], ["Images/PlayerRIGHT - 1.png", "Images/PlayerRIGHT - 2.png"], ["Images/PlayerLEFT - 1.png", "Images/PlayerLEFT - 2.png"], ["Images/PlayerDOWN - 1.png", "Images/PlayerDOWN - 2.png"]], 4)
+var player = new GIF(starting_position, starting_y_position, [["Images/PlayerUP - 1.png", "Images/PlayerUP - 2.png"], ["Images/PlayerRIGHT - 1.png", "Images/PlayerRIGHT - 2.png"], ["Images/PlayerLEFT - 1.png", "Images/PlayerLEFT - 2.png"], ["Images/PlayerDOWN - 1.png", "Images/PlayerDOWN - 2.png"]], 4)
 var pac_man_circle = new GameObject(player.x - 5, player.y - 10, "Images/Circle.png")
 
 player.lives = 3
@@ -150,6 +197,40 @@ function keyCheck(){
     }
 }
 
+function player_move(){
+    if (keyBeingPressed == "rightPressed"){
+        eval("var can_move = nodes.has(node" + (player.x + speed) + player.y + ")")
+        if (can_move){
+            player.x += speed
+            player.degrees = 90
+        }
+    }
+    else if (keyBeingPressed == "leftPressed"){
+        eval("var can_move = nodes.has(node" + (player.x - speed) + player.y + ")")
+        if (can_move){
+            player.x -= speed
+            player.degrees = 270
+        }
+    }
+    else if (keyBeingPressed == "downPressed"){
+        eval("var can_move = nodes.has(node" + player.x + (player.y + speed) + ")")
+        if (can_move){
+            player.y += speed
+            player.degrees = 180
+        } 
+    }
+    else if (keyBeingPressed == "upPressed"){
+        eval("var can_move = nodes.has(node" + player.x + (player.y - speed) + ")")
+        if (can_move){
+            player.y -= speed
+            player.degrees = 0
+        }
+    }
+    if (can_move){
+        hasStarted = true
+    }
+}
+
 function draw(){
     if (!running){
         ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -159,27 +240,20 @@ function draw(){
         if (!hasStarted){
             pac_man_circle.draw()
         }
-        for (let node of nodes){
-            if (node[0] === potential_move[0] && node[1] === potential_move[1]){
-                hasStarted = true
-                if (keyBeingPressed == "rightPressed"){
-                    player.x += speed
-                    player.degrees = 90
-                }
-                else if (keyBeingPressed == "leftPressed"){
-                    player.x -= speed
-                    player.degrees = 270
-                }
-                else if (keyBeingPressed == "downPressed"){
-                    player.y += speed
-                    player.degrees = 180
-                }
-                else if (keyBeingPressed == "upPressed"){
-                    player.y -= speed
-                    player.degrees = 0
-                }
+        try {
+            player_move()
+            previous_key = keyBeingPressed
+        }
+        catch (error){
+            try {
+                keyBeingPressed = previous_key
+                player_move()
+            }  
+            catch (error){
             }
-            ctx.fillRect(node[0], node[1], 1, 1)
+        }
+        for (let node of nodes){
+            ctx.fillRect(node.x, node.y, 1, 1)
         }
         ctx.fill()
         ctx.closePath()
